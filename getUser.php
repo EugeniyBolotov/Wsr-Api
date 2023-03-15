1e5784e793bff5e8c77c99766459b2cd357cb796
EugeniyBolotov <eugene.bolotov2@gmail.com>
2023-03-15 22:52:51 +0500

    Update getUser.php

--- getUser.php
+++ getUser.php
@@ -1,5 +1,5 @@
 <?php
-	$mysqli = new mysqli('bh8pzn9jfci7qpzb9jo2-mysql.services.clever-cloud.com', 'urnoqaplantssriq', 'urnoqaplantssriq', 'wsr_food');
+	$mysqli = new mysqli('bh8pzn9jfci7qpzb9jo2-mysql.services.clever-cloud.com', 'urnoqaplantssriq', 's2dYOEs9F5acGiqTXVoS', 'wsr_food');
 	$mysqli->query("SET NAMES UTF8");
 
 	$query_user = $mysqli->query("select * from `Users`;");
